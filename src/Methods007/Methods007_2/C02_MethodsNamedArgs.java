5c799403df8cc4fe3edb6d32ce060e1935a614ae
EgorKorolevskiy <korollevskiy@gmail.com>
2022-07-03 19:43:11 +0300

    Урок 7+8 + дз

--- src/Methods007/Methods007_2/C02_MethodsNamedArgs.java
+++ src/Methods007/Methods007_2/C02_MethodsNamedArgs.java
@@ -10,7 +10,7 @@ public class C02_MethodsNamedArgs {
     public static void main(String[] args) {
         // Можно вызвать метод так:
         int difference;
-        difference = difference(6, 5);
+        difference = difference(6,5);
 
         // difference = difference(5, 6);
 
